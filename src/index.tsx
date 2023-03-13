ac759e3fd5e5b52867a83e09707f5812e0ac8925
Lewis Storey <lewis.storey@bijc.co.uk>
2023-03-13 16:13:27 +0000

    Added tailwind to project
    Base project now complete

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import "./app.css";
 
 const App = () => (
-    <h1 className="app-heading">BG55 Forza Tuner</h1>
+    <h1 className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">BG55 Forza Tuner</h1>
 );
 
 ReactDOM.render(
